2206436a251824dbfb0ef3f8029334c6a7af7c02
topdeveloper <topdeveloper0908@gmail.com>
2024-04-15 14:54:03 +0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Sticky Header
+
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("myHeader");
+var header_wrapper = document.getElementById("myHeader-wrapper");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > 93) {
+        header.classList.add("sticky-header");
+        header_wrapper.style.height = "93px";
+        
+    } else {
+        header.classList.remove("sticky-header");
+        header_wrapper.style.height = "auto";
+    }
+}
+
+$( document ).ready(function() {
+    myFunction();
+});
